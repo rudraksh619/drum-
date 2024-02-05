2a16baa2448fbbf53d4b449549d6e6d79d9c9fbb
rudraksh619 <rudrakshchandel5@gmail.com>
2024-02-05 20:23:54 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,77 @@
-var limit=( document.querySelectorAll("button").length);
+
+
+
+var limit=document.querySelectorAll("button").length;
 
 for(var i=0;i<limit;i++)
 {
-    // var tune = "./sounds/tom-"+1+".mp3";
-    document.querySelectorAll("button")[i].addEventListener("click",function ()
+    document.querySelectorAll("button")[i].addEventListener("click",function()
     {
-        // var audio =new Audio("sounds/crash.mp3");
-        // audio.play();
-       var which=(this.innerHTML);
-       var tune=("sounds/"+which+".mp3");
-       var audio=new Audio(tune);
-       audio.play();
-  
-    });
+        var which=this.innerHTML;
+        var tune="sounds/"+which+".mp3";
+        var audio=new Audio(tune);
+        audio.play();
+    })
 }
 
+function makesound(key)
+{
+    
+    switch(key){
+       
+        case "w":
+            var tune="sounds/"+"w"+".mp3";
+            var audio=new Audio(tune);
+            audio.play();
+            break;
+
+            case "a":
+                var tune="sounds/"+"a"+".mp3";
+                var audio=new Audio(tune);
+                audio.play();
+                break;
+                
+
+
+
+                case "s":
+                    var tune="sounds/"+"s"+".mp3";
+                    var audio=new Audio(tune);
+                    audio.play();
+                    break;
+
+
+                    case "d":
+                        var tune="sounds/"+"d"+".mp3";
+                        var audio=new Audio(tune);
+                        audio.play();
+                        break;
+
+                        case "j":
+                            var tune="sounds/"+"j"+".mp3";
+                            var audio=new Audio(tune);
+                            audio.play();
+                            break;
+
+
+
+                            case "k":
+                                var tune="sounds/"+"k"+".mp3";
+                                var audio=new Audio(tune);
+                                audio.play();
+                                break;
+
+                                case "l":
+                                    var tune="sounds/"+"l"+".mp3";
+                                    var audio=new Audio(tune);
+                                    audio.play();
+                                    break;
+
+    }
+}
+
+document.addEventListener("keypress",function(event)
+{
+   console.log(event);
+    makesound(event.key);
+})
